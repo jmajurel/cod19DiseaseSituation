22b1f14502d85a1853184a2c53423ded090210cb
jmajurel <jeanfelix.majurel@gmail.com>
2020-03-15 17:40:00 +0100

    fixed issue on dotenv file

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const dotenv = require("dotenv");
-const { getLastestSituation, insertSituation, deleteSituation } = require('./handlers/handleSituation');
-dotenv.config()
+require("dotenv").config();
+const {
+  getLastestSituation,
+  insertSituation,
+  deleteSituation
+} = require("./handlers/handleSituation");
 //dotenv.config({ path: __dirname + "/.env" });
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(bodyParser.json());
-app.route('/situations')
+app
+  .route("/situations")
   .get(getLastestSituation)
-  .post(insertSituation)
-app.delete("/situations/:id",deleteSituation);
+  .post(insertSituation);
+app.delete("/situations/:id", deleteSituation);
 
-
-app.listen(PORT, () => { 
-	console.log(`cod-19DiseaseSituation app is listenning on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`cod-19DiseaseSituation app is listenning on port ${PORT}`);
 });
-
-
--- models/index.js
+++ models/index.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 
-  //`mongodb://j.majurel:Moi23011990@ds247121.mlab.com:47121/cod19-situation` ||
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
 
-mongoose.connect('mongodb://127.0.0.1:27017/cod19-situation',
+mongoose.connect(
+  `mongodb://${process.env.DB_USER}:${process.env.DB_PWD}@ds247121.mlab.com:47121/cod19-situation` ||
+    "mongodb://127.0.0.1:27017/cod19-situation",
   { useNewUrlParser: true }
 );
 
