46d0c4f65359f575bb969ffedc9994885a2f70c0
jmajurel <jeanfelix.majurel@gmail.com>
2020-03-18 21:50:17 +0100

    added more data

--- load10_3Data.sh
+++
@@ -1,8 +0,0 @@
-API_URL="https://cod19-situation-dev.herokuapp.com/situations/"
-
-http POST $API_URL countryName="Italy" timeStamp='3/10/2020, 10:00:00 AM' activeCase:=9172 newCase:=1797 totalDeaths:=463 newDeaths:=97
-http POST $API_URL countryName="France" timeStamp='3/10/2020, 10:00:00 AM' activeCase:=1402 newCase:=286 totalDeaths:=30 newDeaths:=11
-http POST $API_URL countryName="China" timeStamp='3/10/2020, 10:00:00 AM' activeCase:=80924 newCase:=20 totalDeaths:=3140 newDeaths:=17
-http POST $API_URL countryName="United States of America" timeStamp='3/10/2020, 10:00:00 AM' activeCase:=472 newCase:=259 totalDeaths:=19 newDeaths:=8
-   
-     
--- load15_3Data.sh
+++
@@ -1,6 +0,0 @@
-API_URL="https://cod19-situation-dev.herokuapp.com/situations/"
-
-http POST $API_URL countryName="Italy" timeStamp='3/15/2020, 10:00:00 AM' activeCase:=24747 newCase:=3590 totalDeaths:=1809 newDeaths:=368
-http POST $API_URL countryName="France" timeStamp='3/15/2020, 10:00:00 AM' activeCase:=5380 newCase:=911 totalDeaths:=127 newDeaths:=36
-http POST $API_URL countryName="China" timeStamp='3/15/2020, 10:00:00 AM' activeCase:=81077 newCase:=29 totalDeaths:=3218 newDeaths:=14
-http POST $API_URL countryName="United States of America" timeStamp='3/15/2020, 10:00:00 AM' activeCase:=1678 newCase:=0 totalDeaths:=41 newDeaths:=0
