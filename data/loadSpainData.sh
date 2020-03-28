13b14b68f328125211fa4f6965b0f72990c4f49e
jmajurel <jeanfelix.majurel@gmail.com>
2020-03-28 11:09:36 +0100

    added spain and uk data

---
+++ data/loadSpainData.sh
@@ -0,0 +1,29 @@
+API_URL='https://cod19-situation-dev.herokuapp.com/situations/'
+
+http POST $API_URL countryName='Spain' timeStamp='3/27/2020, 10:00:00 AM' activeCase:=56188 newCase:=8578 totalDeaths:=4089 newDeaths:=655
+http POST $API_URL countryName='Spain' timeStamp='3/26/2020, 10:00:00 AM' activeCase:=47610 newCase:=7937 totalDeaths:=3434 newDeaths:=738
+http POST $API_URL countryName='Spain' timeStamp='3/25/2020, 10:00:00 AM' activeCase:=39673 newCase:=6584 totalDeaths:=2696 newDeaths:=514
+http POST $API_URL countryName='Spain' timeStamp='3/24/2020, 10:00:00 AM' activeCase:=33089 newCase:=4517 totalDeaths:=2182 newDeaths:=462
+http POST $API_URL countryName='Spain' timeStamp='3/23/2020, 10:00:00 AM' activeCase:=28572 newCase:=3646 totalDeaths:=1720 newDeaths:=394
+http POST $API_URL countryName='Spain' timeStamp='3/21/2020, 10:00:00 AM' activeCase:=24926 newCase:=4946 totalDeaths:=1326 newDeaths:=324
+http POST $API_URL countryName='Spain' timeStamp='3/20/2020, 10:00:00 AM' activeCase:=19980 newCase:=2833 totalDeaths:=1002 newDeaths:=235
+http POST $API_URL countryName='Spain' timeStamp='3/19/2020, 10:00:00 AM' activeCase:=17147 newCase:=3431 totalDeaths:=767 newDeaths:=169
+http POST $API_URL countryName='Spain' timeStamp='3/18/2020, 10:00:00 AM' activeCase:=11178 newCase:=1987 totalDeaths:=491 newDeaths:=182
+http POST $API_URL countryName='Spain' timeStamp='3/17/2020, 10:00:00 AM' activeCase:=9191 newCase:=1438 totalDeaths:=309 newDeaths:=21
+http POST $API_URL countryName='Spain' timeStamp='3/16/2020, 10:00:00 AM' activeCase:=7753 newCase:=2000 totalDeaths:=288 newDeaths:=152
+http POST $API_URL countryName='Spain' timeStamp='3/15/2020, 10:00:00 AM' activeCase:=5753 newCase:=1522 totalDeaths:=136 newDeaths:=16
+http POST $API_URL countryName='Spain' timeStamp='3/14/2020, 10:00:00 AM' activeCase:=4231 newCase:=1266 totalDeaths:=120 newDeaths:=36
+http POST $API_URL countryName='Spain' timeStamp='3/13/2020, 10:00:00 AM' activeCase:=2965 newCase:=825 totalDeaths:=84 newDeaths:=36
+http POST $API_URL countryName='Spain' timeStamp='3/12/2020, 10:00:00 AM' activeCase:=2140 newCase:=501 totalDeaths:=48 newDeaths:=12
+http POST $API_URL countryName='Spain' timeStamp='3/11/2020, 10:00:00 AM' activeCase:=1639 newCase:=615 totalDeaths:=36 newDeaths:=8
+http POST $API_URL countryName='Spain' timeStamp='3/10/2020, 10:00:00 AM' activeCase:=1024 newCase:=435 totalDeaths:=28 newDeaths:=18
+http POST $API_URL countryName='Spain' timeStamp='3/10/2020, 10:00:00 AM' activeCase:=1024 newCase:=435 totalDeaths:=28 newDeaths:=18
+http POST $API_URL countryName='Spain' timeStamp='3/09/2020, 10:00:00 AM' activeCase:=589 newCase:=159 totalDeaths:=10 newDeaths:=5
+http POST $API_URL countryName='Spain' timeStamp='3/08/2020, 10:00:00 AM' activeCase:=430 newCase:=56 totalDeaths:=5 newDeaths:=0
+http POST $API_URL countryName='Spain' timeStamp='3/07/2020, 10:00:00 AM' activeCase:=374 newCase:=117 totalDeaths:=5 newDeaths:=2
+http POST $API_URL countryName='Spain' timeStamp='3/06/2020, 10:00:00 AM' activeCase:=257 newCase:=59 totalDeaths:=3 newDeaths:=2
+http POST $API_URL countryName='Spain' timeStamp='3/05/2020, 10:00:00 AM' activeCase:=198 newCase:=47 totalDeaths:=1 newDeaths:=1
+http POST $API_URL countryName='Spain' timeStamp='3/04/2020, 10:00:00 AM' activeCase:=151 newCase:=37 totalDeaths:=0 newDeaths:=0
+http POST $API_URL countryName='Spain' timeStamp='3/03/2020, 10:00:00 AM' activeCase:=114 newCase:=69 totalDeaths:=0 newDeaths:=0
+http POST $API_URL countryName='Spain' timeStamp='3/02/2020, 10:00:00 AM' activeCase:=45 newCase:=0 totalDeaths:=0 newDeaths:=0
+http POST $API_URL countryName='Spain' timeStamp='3/01/2020, 10:00:00 AM' activeCase:=45 newCase:=13 totalDeaths:=0 newDeaths:=0
---
+++ data/loadUkData.sh
@@ -0,0 +1,28 @@
+API_URL='https://cod19-situation-dev.herokuapp.com/situations/'
+
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/27/2020, 10:00:00 AM' activeCase:=11662 newCase:=2129 totalDeaths:=578 newDeaths:=115
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/26/2020, 10:00:00 AM' activeCase:=9533 newCase:=1452 totalDeaths:=463 newDeaths:=41
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/25/2020, 10:00:00 AM' activeCase:=8081 newCase:=1427 totalDeaths:=422 newDeaths:=87
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/24/2020, 10:00:00 AM' activeCase:=6654 newCase:=967 totalDeaths:=335 newDeaths:=54
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/23/2020, 10:00:00 AM' activeCase:=5687 newCase:=669 totalDeaths:=281 newDeaths:=48
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/21/2020, 10:00:00 AM' activeCase:=5018 newCase:=1035 totalDeaths:=233 newDeaths:=56
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/20/2020, 10:00:00 AM' activeCase:=3983 newCase:=706 totalDeaths:=177 newDeaths:=33
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/19/2020, 10:00:00 AM' activeCase:=3277 newCase:=647 totalDeaths:=144 newDeaths:=41
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/18/2020, 10:00:00 AM' activeCase:=1954 newCase:=407 totalDeaths:=55 newDeaths:=5
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/17/2020, 10:00:00 AM' activeCase:=1547 newCase:=152 totalDeaths:=55 newDeaths:=20
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/16/2020, 10:00:00 AM' activeCase:=1395 newCase:=251 totalDeaths:=35 newDeaths:=14
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/15/2020, 10:00:00 AM' activeCase:=1144 newCase:=342 totalDeaths:=21 newDeaths:=11
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/14/2020, 10:00:00 AM' activeCase:=802 newCase:=208 totalDeaths:=10 newDeaths:=2
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/13/2020, 10:00:00 AM' activeCase:=594 newCase:=134 totalDeaths:=8 newDeaths:=2
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/12/2020, 10:00:00 AM' activeCase:=460 newCase:=87 totalDeaths:=6 newDeaths:=0
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/11/2020, 10:00:00 AM' activeCase:=373 newCase:=50 totalDeaths:=6 newDeaths:=3
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/10/2020, 10:00:00 AM' activeCase:=323 newCase:=46 totalDeaths:=3 newDeaths:=1
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/09/2020, 10:00:00 AM' activeCase:=277 newCase:=67 totalDeaths:=2 newDeaths:=0
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/08/2020, 10:00:00 AM' activeCase:=210 newCase:=43 totalDeaths:=2 newDeaths:=1
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/07/2020, 10:00:00 AM' activeCase:=167 newCase:=49 totalDeaths:=1 newDeaths:=1
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/06/2020, 10:00:00 AM' activeCase:=118 newCase:=29 totalDeaths:=0 newDeaths:=0
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/05/2020, 10:00:00 AM' activeCase:=89 newCase:=38 totalDeaths:=0 newDeaths:=0
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/04/2020, 10:00:00 AM' activeCase:=51 newCase:=12 totalDeaths:=0 newDeaths:=0
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/03/2020, 10:00:00 AM' activeCase:=39 newCase:=3 totalDeaths:=0 newDeaths:=0
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/02/2020, 10:00:00 AM' activeCase:=36 newCase:=13 totalDeaths:=0 newDeaths:=0
+http POST $API_URL countryName='The United Kingdom' timeStamp='3/01/2020, 10:00:00 AM' activeCase:=18 newCase:=8 totalDeaths:=0 newDeaths:=0
