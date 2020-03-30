af8eee8cb90d5efa6c00b84d794a5a41d5d3237a
jmajurel <jeanfelix.majurel@gmail.com>
2020-03-30 13:56:38 +0200

    added more data

---
+++ data/loadChineaseData.sh
@@ -0,0 +1,17 @@
+API_URL='https://cod19-situation-dev.herokuapp.com/situations/'
+
+http POST $API_URL countryName='China' timeStamp='3/06/2020, 10:00:00 AM' activeCase:=80711 newCase:=146 totalDeaths:=3045 newDeaths:=30
+http POST $API_URL countryName='China' timeStamp='3/05/2020, 10:00:00 AM' activeCase:=80565 newCase:=143 totalDeaths:=3015 newDeaths:=31
+http POST $API_URL countryName='China' timeStamp='3/04/2020, 10:00:00 AM' activeCase:=80422 newCase:=120 totalDeaths:=2984 newDeaths:=38
+http POST $API_URL countryName='China' timeStamp='3/03/2020, 10:00:00 AM' activeCase:=80304 newCase:=130 totalDeaths:=2946 newDeaths:=31
+http POST $API_URL countryName='China' timeStamp='3/02/2020, 10:00:00 AM' activeCase:=80174 newCase:=206 totalDeaths:=2915 newDeaths:=42
+http POST $API_URL countryName='China' timeStamp='3/01/2020, 10:00:00 AM' activeCase:=79968 newCase:=579 totalDeaths:=2873 newDeaths:=35
+http POST $API_URL countryName='China' timeStamp='2/29/2020, 10:00:00 AM' activeCase:=79394 newCase:=435 totalDeaths:=2838 newDeaths:=47
+http POST $API_URL countryName='China' timeStamp='2/28/2020, 10:00:00 AM' activeCase:=78961 newCase:=331 totalDeaths:=2791 newDeaths:=44
+http POST $API_URL countryName='China' timeStamp='2/27/2020, 10:00:00 AM' activeCase:=78630 newCase:=439 totalDeaths:=2747 newDeaths:=29
+http POST $API_URL countryName='China' timeStamp='2/26/2020, 10:00:00 AM' activeCase:=78191 newCase:=412 totalDeaths:=2718 newDeaths:=52
+http POST $API_URL countryName='China' timeStamp='2/25/2020, 10:00:00 AM' activeCase:=77780 newCase:=518 totalDeaths:=2666 newDeaths:=71
+http POST $API_URL countryName='China' timeStamp='2/24/2020, 10:00:00 AM' activeCase:=77262 newCase:=415 totalDeaths:=2595 newDeaths:=150
+http POST $API_URL countryName='China' timeStamp='2/23/2020, 10:00:00 AM' activeCase:=77042 newCase:=650 totalDeaths:=2445 newDeaths:=97
+http POST $API_URL countryName='China' timeStamp='2/22/2020, 10:00:00 AM' activeCase:=76392 newCase:=397 totalDeaths:=2348 newDeaths:=109
+http POST $API_URL countryName='China' timeStamp='2/21/2020, 10:00:00 AM' activeCase:=75569 newCase:=894 totalDeaths:=2239 newDeaths:=118
---
+++ data/loadKoreanData.sh
@@ -0,0 +1,21 @@
+API_URL='https://cod19-situation-dev.herokuapp.com/situations/'
+
+http POST $API_URL countryName='Republic of Korea' timeStamp='3/06/2020, 10:00:00 AM' activeCase:=6284 newCase:=518 totalDeaths:=42 newDeaths:=7
+http POST $API_URL countryName='Republic of Korea' timeStamp='3/05/2020, 10:00:00 AM' activeCase:=5766 newCase:=438 totalDeaths:=35 newDeaths:=3
+http POST $API_URL countryName='Republic of Korea' timeStamp='3/04/2020, 10:00:00 AM' activeCase:=5328 newCase:=516 totalDeaths:=32 newDeaths:=4
+http POST $API_URL countryName='Republic of Korea' timeStamp='3/03/2020, 10:00:00 AM' activeCase:=4812 newCase:=600 totalDeaths:=28 newDeaths:=6
+http POST $API_URL countryName='Republic of Korea' timeStamp='3/02/2020, 10:00:00 AM' activeCase:=4212 newCase:=476 totalDeaths:=22 newDeaths:=4
+http POST $API_URL countryName='Republic of Korea' timeStamp='3/01/2020, 10:00:00 AM' activeCase:=3736 newCase:=586 totalDeaths:=18 newDeaths:=1
+http POST $API_URL countryName='Republic of Korea' timeStamp='2/29/2020, 10:00:00 AM' activeCase:=3150 newCase:=813 totalDeaths:=17 newDeaths:=4
+http POST $API_URL countryName='Republic of Korea' timeStamp='2/28/2020, 10:00:00 AM' activeCase:=2337 newCase:=571 totalDeaths:=13 newDeaths:=0
+http POST $API_URL countryName='Republic of Korea' timeStamp='2/27/2020, 10:00:00 AM' activeCase:=1766 newCase:=505 totalDeaths:=13 newDeaths:=1
+http POST $API_URL countryName='Republic of Korea' timeStamp='2/26/2020, 10:00:00 AM' activeCase:=1261 newCase:=284 totalDeaths:=12 newDeaths:=2
+http POST $API_URL countryName='Republic of Korea' timeStamp='2/25/2020, 10:00:00 AM' activeCase:=977 newCase:=214 totalDeaths:=10 newDeaths:=3
+http POST $API_URL countryName='Republic of Korea' timeStamp='2/24/2020, 10:00:00 AM' activeCase:=763 newCase:=161 totalDeaths:=7 newDeaths:=2
+http POST $API_URL countryName='Republic of Korea' timeStamp='2/23/2020, 10:00:00 AM' activeCase:=602 newCase:=256 totalDeaths:=5 newDeaths:=3
+http POST $API_URL countryName='Republic of Korea' timeStamp='2/22/2020, 10:00:00 AM' activeCase:=346 newCase:=142 totalDeaths:=2 newDeaths:=1
+http POST $API_URL countryName='Republic of Korea' timeStamp='2/21/2020, 10:00:00 AM' activeCase:=204 newCase:=100 totalDeaths:=1 newDeaths:=0
+
+
+
+
